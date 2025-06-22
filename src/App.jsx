3f60ee3faf91437a4abdf6735e0df57cf2924cb2
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-22 22:46:53 +0100

    New Component Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import './App.css'
 function App() {
- 
- 
   return (
     <>
-      
       <h1>Vite + React</h1>
       <Person></Person>
       <Person></Person>
       <Person></Person>
-
-     
+      <Sports></Sports>
     </>
   )
 }
-
-function Person(){
-   let age = 45;
-  return(
-    <p>I am a person -{age}</p>
+function Person() {
+  let age = 45;
+  let name = 'Jack'
+  return (
+    <p>I am a person:{name} -{age}</p>
   )
 }
-
+function Sports() {
+  return (
+    <>
+      <h2>Cricket</h2>
+      <p>Playing and loosing</p>
+    </>
+  )
+}
 export default App
